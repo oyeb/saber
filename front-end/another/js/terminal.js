113e02feeebe5dd9d748441c05d03ee6c4c0befb
arrow- <ananya95@gmail.com>
2016-01-15 22:23:57 +0530

    Clean commits

--- front-end/another/js/terminal.js
+++ front-end/another/js/terminal.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
 		var type = function () {
 			$('#story').typed({
 		                strings: command,
-		                typeSpeed: 20,
+		                typeSpeed: 5,
 		                backDelay: 500,
 		                loop: false,
 		                loopCount: false,
@@ -31,6 +31,9 @@ $(document).ready(function() {
 				
 			}else if($('#input').val() == "clear"){
 				command = [">>"];
+				type();
+			}else if($('#input').val() == "help"){
+				command = ["Brolog has nothing to do with Prolog, the language of AI.^10<br>bob is not an AI,^300 but it understands Brolog.^50<br>You must learn Brolog.<br>Try `learn`<br>>>"];
 				type();
 			}else{
 				command = [">> Enter a valid command<br>>>"];
