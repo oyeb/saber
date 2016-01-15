9a04c2c558cf25a502124c27c46d1f6ef498d045
Sourav <jsourav01@gmail.com>
2016-01-16 03:25:49 +0530

    Everything is done.Just fix the heading of one of the box in the index.html. and sorry for wasting your time

--- front-end/final/assets/js/main.js
+++ front-end/final/assets/js/main.js
@@ -109,6 +109,34 @@
 			// Colorbox Modal
 			$(".CBmodal").colorbox({inline:true, initialWidth:100, maxWidth:682, initialHeight:100, transition:"elastic",speed:750});
 
+
+			var nt_example2 = $('#bob').newsTicker({
+	row_height: 60,
+	max_rows: 1,
+	speed: 300,
+	duration: 6000,
+	prevButton: $('#bob-prev'),
+	nextButton: $('#bob-next'),
+	hasMoved: function() {
+    	$('#bob-infos-container').fadeOut(200, function(){
+        	$('#bob-infos .infos-hour').text($('#bob li:first span').text());
+        	$('#bob-infos .infos-text').text($('#bob li:first').data('infos'));
+        	$(this).fadeIn(400);
+        });
+    },
+    pause: function() {
+    	$('#bob li i').removeClass('fa-play').addClass('fa-pause');
+    },
+    unpause: function() {
+    	$('#bob li i').removeClass('fa-pause').addClass('fa-play');
+    }
+});
+$('#bob-infos').hover(function() {
+    nt_example2.newsTicker('pause');
+}, function() {
+    nt_example2.newsTicker('unpause');
+});
+
 	});
 
 })(jQuery);
